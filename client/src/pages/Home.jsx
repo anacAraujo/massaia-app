d74a53c12bbb093eb36411ee82ef96cba92b893a
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-04-29 21:38:37 +0100

    feat: create building page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,14 +4,14 @@ export function Home() {
   return (
     <div className="videoPlayer">
       <div className="overlay"></div>
-      <video src="../upload/massaia.mp4" autoPlay loop muted />
+      <video src="../upload/massaiaBuild.mp4" autoPlay loop muted />
       <img className="img-eyes" src="../assets/images/olhos.png" />
       <div className="content">
         <h1>MASSAIÁ</h1>
-        <p>espaços da voz, do som e do olhar</p>
+        {/*<p>espaços da voz, do som e do olhar</p>*/}
       </div>
       <div className="construction-notice">
-        <h2>em costrução</h2>
+        <p>em costrução</p>
       </div>
     </div>
   );
