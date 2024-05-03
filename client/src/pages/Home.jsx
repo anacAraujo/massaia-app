cc46c8d3fb54f80f26fa86ba6727a90447defe18
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-03 21:22:38 +0100

    feat: add state to control visibility of img-eyes and content elements

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function Home() {
+  const [isLandingCompVisible, setLandingCompVisible] = useState(true);
+
+  const handleVideoPlayerClick = () => {
+    setLandingCompVisible(false);
+  };
+
   return (
-    <div className="videoPlayer">
+    <div className="videoPlayer" onClick={handleVideoPlayerClick}>
       <div className="overlay"></div>
       <video src="../upload/massaiaBuild.mp4" autoPlay loop muted />
-      <img className="img-eyes" src="../assets/images/olhos.png" />
-      <div className="content">
-        <h1>MASSAIÁ</h1>
-        <p>espaços da voz, do som e do olhar</p>
-      </div>
+      {isLandingCompVisible && (
+        <>
+          <img className="img-eyes" src="../assets/images/olhos.png" />
+          <div className="content">
+            <h1>MASSAIÁ</h1>
+            <p>espaços da voz, do som e do olhar</p>
+          </div>
+        </>
+      )}
       {/* <div className="construction-notice">
         <p>em costrução</p>
       </div> */}
