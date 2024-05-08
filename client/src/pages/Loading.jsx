4350949ea7f619ef04d35b8f7a05becabacae4a6
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-08 19:08:04 +0100

    feat: create loaging page

---
+++ client/src/pages/Loading.jsx
@@ -0,0 +1,9 @@
+import "../styles/landingPage.css";
+
+export function Loading() {
+  return (
+    <div className="loagingPage">
+      <img src="../assets/images/olhos.png" />
+    </div>
+  );
+}
