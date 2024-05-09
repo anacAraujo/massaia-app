fe52f2f1bf3bb79c554b611c0bc1bd75dce599d7
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-09 18:19:49 +0100

    feat: add menu dropdowns

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -1,25 +1,67 @@
+import React, { useState } from "react";
 import "../styles/menu.css";
 
 export function Menu() {
+  const [selectedMenu, setSelectedMenu] = useState(null);
+
+  const handleMenuClick = (menu) => {
+    setSelectedMenu(selectedMenu === menu ? null : menu);
+  };
+
   return (
-    <div className="menu">
-      <div className="menu-header">
+    <div>
+      <div>
         <h1 className="menu-massaia">MASSAIÁ</h1>
         <img className="menu-exit" src="../assets/icons/exit.svg" alt="exit" />
       </div>
 
       <div>
         <div className="menu-options">
-          <div className="menu-option">
+          <div
+            className="menu-main-option"
+            onClick={() => handleMenuClick("videos")}
+          >
             <p>vídeos</p>
+            <div
+              className="menu-option"
+              style={{ display: selectedMenu === "videos" ? "block" : "none" }}
+            >
+              <p>volume I</p>
+              <p>volume II</p>
+            </div>
           </div>
-          <div className="menu-option">
+          <div
+            className="menu-main-option"
+            onClick={() => handleMenuClick("galeria")}
+          >
             <p>galeria</p>
+            <div
+              className="menu-option"
+              style={{ display: selectedMenu === "galeria" ? "block" : "none" }}
+            >
+              <p>volume I</p>
+              <p>volume II</p>
+            </div>
           </div>
-          <div className="menu-option">
+          <div
+            className="menu-main-option"
+            onClick={() => handleMenuClick("sobre")}
+          >
             <p>sobre</p>
+            <div
+              className="menu-option"
+              style={{ display: selectedMenu === "sobre" ? "block" : "none" }}
+            >
+              <p>projetos</p>
+              <p>autores</p>
+              <p>artistas</p>
+              <p>site</p>
+            </div>
           </div>
-          <div className="menu-option">
+          <div
+            className="menu-main-option"
+            onClick={() => handleMenuClick("momentos")}
+          >
             <p>momentos</p>
           </div>
         </div>
