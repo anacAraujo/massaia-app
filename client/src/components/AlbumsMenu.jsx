6835268f6d6139ca2022427c5f1767b3bf4da7be
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-15 18:48:44 +0100

    feat: show albums covers from request

--- client/src/components/AlbumsMenu.jsx
+++ client/src/components/AlbumsMenu.jsx
@@ -1,7 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "../lib/axiosConfig.js";
+import "../styles/homeMenus.css";
+
 export function AlbumsMenu() {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/albums`);
+        setAlbums(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  console.log("albums: ", albums);
+
   return (
     <div>
-      <p>AlbumsMenu</p>
+      {albums.map((album) => (
+        <div key={album.id} className="menu-albums">
+          <img src={`../upload/${album.cover}`} alt="album cover" />
+          <img src="" />
+        </div>
+      ))}
     </div>
   );
 }
