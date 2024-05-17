e3b953b4d5eda8e51d18b192648aa10fdf67ff27
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-17 17:25:21 +0100

    feat: create moments page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Menu } from "./pages/Menu";
 import { Credits } from "./pages/Credits";
+import { Moments } from "./pages/Moments";
 import { ViewLandingContextProvider } from "./context/viewLandingContext";
 import { ViewAlbumsMenuProvider } from "./context/viewAlbumsMenu";
 
@@ -16,11 +17,7 @@ function App() {
               <Route path="/" index element={<Home></Home>}></Route>
               <Route path="/menu" element={<Menu></Menu>}></Route>
               <Route path="/credits" element={<Credits></Credits>}></Route>
-              {/* <Route path="/login" element={<Login></Login>}></Route>
-        <Route path="/signup" element={<SignUp></SignUp>}></Route>
-        <Route path="/profile" element={<Profile></Profile>}></Route>
-        <Route path="/breedinfo/:type/:breed" element={<BreedInfo></BreedInfo>}></Route>
-        <Route path="/about" element={<About></About>}></Route> */}
+              <Route path="/momentos" element={<Moments></Moments>}></Route>
             </Route>
           </Routes>
         </HashRouter>
---
+++ client/src/pages/Moments.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "../index.css";
+
+export function Moments() {
+  return (
+    <div>
+      <div className="navbar">
+        <h1 className="navbar-massaia">MASSAIÁ</h1>
+        <Link to="/">
+          {" "}
+          <img
+            className="navbar-menu"
+            src="../assets/icons/menu-black.svg"
+            alt="exit"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+}
