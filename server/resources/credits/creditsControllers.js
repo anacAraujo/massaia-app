2dab1c5d2d473acfc9954564c849eec0e7e8eb6d
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-17 19:23:08 +0100

    feat: create credits controllers

---
+++ server/resources/credits/creditsControllers.js
@@ -0,0 +1,27 @@
+import { db } from "../../db/db.js";
+
+import { idSchema } from "./albumsSchemas.js";
+
+export async function getSongsCredits(req, res, next) {
+  try {
+    const params = await idSchema.validateAsync(req.params);
+
+    let query = `
+        SELECT massaia.authors.name, massaia.roles.name 
+        FROM massaia.songs_has_authors_has_roles 
+            JOIN massaia.songs ON massaia.songs.id = massaia.songs_has_authors_has_roles.songs_id
+            JOIN massaia.authors ON massaia.authors.id = massaia.songs_has_authors_has_roles.authors_id
+            JOIN massaia.roles ON massaia.roles.id = massaia.songs_has_authors_has_roles.roles_id
+        WHERE massaia.songs.id = ?`;
+
+    const [results] = await db.execute(query, [params.id]);
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: "Song not found!" });
+    }
+
+    res.status(200).json(results);
+  } catch (error) {
+    next(error);
+  }
+}
