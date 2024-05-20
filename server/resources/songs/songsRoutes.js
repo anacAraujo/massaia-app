bd58c01a86db5a688f8e00ab72279b70a400d750
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-20 19:06:27 +0100

    add: new route

--- server/resources/songs/songsRoutes.js
+++ server/resources/songs/songsRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getSong } from "./songsControllers.js";
+import { getSong, getSongs } from "./songsControllers.js";
 
 const router = express.Router();
 
+router.get("/", getSongs);
 router.get("/:id", getSong);
 
 export default router;
