5abbcea2b208af4b6dfd5cf483ebe7fbb372c4a2
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-20 19:06:44 +0100

    add: new request

--- server/resources/songs/songsControllers.js
+++ server/resources/songs/songsControllers.js
@@ -1,6 +1,31 @@
+import { query } from "express";
 import { db } from "../../db/db.js";
 
-import { idSchema } from "./songsSchemas.js";
+import { idSchema, albumIddSchema } from "./songsSchemas.js";
+
+export async function getSongs(req, res, next) {
+  try {
+    const params = await albumIddSchema.validateAsync(req.query);
+
+    //TODO
+    let query =
+      "SELECT s.*, a.name AS album_name FROM songs s JOIN albums a ON a.id = s.album_id ORDER BY a.id ASC, s.position ASC";
+
+    if (params.album_id) {
+      query += " WHERE songs.album_id=:album_id";
+    }
+
+    const [results] = await db.execute(query, params);
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: "Songs not found!" });
+    }
+
+    res.status(200).json(results);
+  } catch (error) {
+    next(error);
+  }
+}
 
 export async function getSong(req, res, next) {
   try {
@@ -14,7 +39,7 @@ export async function getSong(req, res, next) {
       return res.status(404).json({ message: "Song not found!" });
     }
 
-    res.status(200).json(results);
+    res.status(200).json(results[0]);
   } catch (error) {
     next(error);
   }
