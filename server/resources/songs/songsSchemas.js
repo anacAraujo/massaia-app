6cb2f1cd8f9e7b408bd21a9043e0ba8355c96188
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-20 19:06:13 +0100

    add: new song schema

--- server/resources/songs/songsSchemas.js
+++ server/resources/songs/songsSchemas.js
@@ -1,7 +1,11 @@
 import Joi from "joi";
 
-const id = Joi.number().integer().positive().required();
+const id = Joi.number().integer().positive();
 
 export const idSchema = Joi.object({
-  id: id,
+  id: id.optional(),
+});
+
+export const albumIddSchema = Joi.object({
+  album_id: id.optional(),
 });
