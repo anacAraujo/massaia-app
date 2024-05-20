bf4dac3c8636ec9fbf769fdc933c57bb4544210b
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-20 19:10:36 +0100

    add: TODOs

--- client/src/context/viewLandingContext.js
+++ client/src/context/viewLandingContext.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 
+// TODO create current song context
+
+// TODO create albumns info context
+
 export const ViewLandingContext = React.createContext();
 
+// TODO delete and use state instead of context
 export function ViewLandingContextProvider({ children }) {
   const [hasViewedLandingPage, setHasViewedLandingPage] = useState(false);
 
   function handleHasViewedLandingPage(hasViewedLandingPage) {
     setHasViewedLandingPage(hasViewedLandingPage);
-    localStorage.setItem(
-      "hasViewedLandingPage",
-      JSON.stringify(hasViewedLandingPage)
-    );
   }
 
   useEffect(() => {
