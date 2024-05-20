ece16df47c4c439ff5efca06c88944ff784645b5
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-20 19:07:59 +0100

    fix: query to also get songs info

--- server/resources/albums/albumsControllers.js
+++ server/resources/albums/albumsControllers.js
@@ -9,7 +9,7 @@ export async function getAlbums(req, res, next) {
     const [results] = await db.execute(query);
 
     if (results.length === 0) {
-      return res.status(404).json({ message: "Albums not found!" });
+      return res.status(404).json({ message: "Album not found!" });
     }
 
     res.status(200).json(results);
@@ -22,7 +22,8 @@ export async function getAlbum(req, res, next) {
   try {
     const params = await idSchema.validateAsync(req.params);
 
-    let query = "SELECT * FROM albums WHERE id = ?";
+    let query =
+      "SELECT * FROM albums JOIN songs ON albums.id = songs.album_id WHERE albums.id = ?";
 
     const [results] = await db.execute(query, [params.id]);
 
