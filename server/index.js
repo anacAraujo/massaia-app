5d12e24c737f00c09101727473344e644009e5f6
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-21 20:17:24 +0100

    fix: route path

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.post("/api/upload", upload.single("file"), function (req, res) {
   res.status(200).json({ filename: file.filename });
 });
 
-app.use("api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/albums/:album_id/songs", songsRoutes);
 app.use("/api/albums", albumsRoutes);
 app.use("/api/songs", songsRoutes);
