9d462b817cadcb6345cc1bdd823a4a9b81509ef3
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-24 15:56:42 +0100

    feat: create component for landing page

---
+++ client/src/components/LandingPage.jsx
@@ -0,0 +1,18 @@
+import "../styles/landingPage.css";
+
+export function LandingPage() {
+  return (
+    <>
+      <div className="eyes">
+        <img className="img-eyes" src="../assets/images/olhos.png" alt="eyes" />
+      </div>
+      <div className="content">
+        <h1>MASSAIÁ</h1>
+        <p>espaços da voz, do som e do olhar</p>
+      </div>
+      <div className="construction-notice">
+        <p>em construção</p>
+      </div>
+    </>
+  );
+}
