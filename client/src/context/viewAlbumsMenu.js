25215f30af414d9dffbc0f368ad3f4fcc4067d78
Ana Araújo <anacristinaferrazaraujo@gmail.com>
2024-05-24 16:19:21 +0100

    chore: delete unused function

--- client/src/context/viewAlbumsMenu.js
+++ client/src/context/viewAlbumsMenu.js
@@ -2,17 +2,8 @@ import React, { useState, useEffect } from "react";
 
 export const ViewAlbumsMenu = React.createContext();
 
-// TODO delete and use state instead of context
 export function ViewAlbumsMenuProvider({ children }) {
   const [isViewingAlbumsMenu, setIsViewingAlbumsMenu] = useState(false);
-
-  function handleIsViewingAlbumsMenu(isViewingAlbumsMenu) {
-    setIsViewingAlbumsMenu(isViewingAlbumsMenu);
-    localStorage.setItem(
-      "isViewingAlbumsMenu",
-      JSON.stringify(isViewingAlbumsMenu)
-    );
-  }
 
   useEffect(() => {
     setIsViewingAlbumsMenu(false);
