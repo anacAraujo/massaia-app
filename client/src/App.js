9a2726335358f5a9c984c69c31511f0bd8dcadc2
anacAraujo <anacristinaferrazaraujo@gmail.com>
2024-06-01 20:22:47 +0100

    fix: add gallery route and change import to default

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-import { Home } from "./pages/Home";
-import { Menu } from "./pages/Menu";
-import { Credits } from "./pages/Credits";
-import { Moments } from "./pages/Moments";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Gallery  from "./pages/Gallery";
+import Credits from "./pages/Credits";
+import Moments from "./pages/Moments";
 import { ViewAlbumsMenuProvider } from "./context/viewAlbumsMenu";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route>
             <Route path="/" index element={<Home></Home>}></Route>
             <Route path="/menu" element={<Menu></Menu>}></Route>
+            <Route path="/gallery" element={<Gallery></Gallery>}></Route>
             <Route path="/credits" element={<Credits></Credits>}></Route>
             <Route path="/momentos" element={<Moments></Moments>}></Route>
           </Route>
