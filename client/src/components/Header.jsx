04b425211f19080370747105a17d106431ed52c5
anacAraujo <anacristinaferrazaraujo@gmail.com>
2024-06-03 15:59:08 +0100

    feat: create header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,1 +1,18 @@
-export default function Header() {}
+import "../styles/menu.css";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div>
+      <h1 className="menu-massaia">MASSAIÁ</h1>
+      <Link to="/">
+        {" "}
+        <img
+          className="menu-exit"
+          src="../assets/icons/menu-black.svg"
+          alt="menu"
+        />
+      </Link>
+    </div>
+  );
+}
