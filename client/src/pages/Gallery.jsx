d5e59f8e4a79d8bfe9206de5a5dd83142d759753
anacAraujo <anacristinaferrazaraujo@gmail.com>
2024-06-03 15:59:29 +0100

    feat: add header and music controllers

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom";
 
+import Header from "../components/Header";
+import MusicControllers from "../components/MusicControllers";
+
 export default function Gallery() {
   const { volume } = useParams();
   console.log("volume: ", volume);
 
   return (
     <di>
+      <Header></Header>
       <div></div>
+      <MusicControllers></MusicControllers>
     </di>
   );
 }
