7b7610bd6e526bdef7a05021e1bd8082c431451d
anacAraujo <anacristinaferrazaraujo@gmail.com>
2024-06-03 16:00:35 +0100

    feat: create component that allows to control music

---
+++ client/src/components/MusicControllers.jsx
@@ -0,0 +1,21 @@
+import "../styles/musicControllers.css";
+
+export default function MusicControllers() {
+  return (
+    <div className="musicControllers">
+      <div>
+        <button className="musicControllers-vol">vol. 1</button>
+        <p className="musicControllers-song-name">nome da música</p>
+        <img
+          className="musicControllers-more-songs"
+          src=""
+          alt="view all songs"
+        />
+      </div>
+
+      <div></div>
+
+      <div></div>
+    </div>
+  );
+}
