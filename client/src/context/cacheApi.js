d8811f060b458e16689ee2a4b86d2c499587a243
anacAraujo <anacristinaferrazaraujo@gmail.com>
2024-06-09 23:52:12 +0100

    feat: move songsByAlbum request to cache api context

--- client/src/context/cacheApi.js
+++ client/src/context/cacheApi.js
@@ -1,20 +1,45 @@
 import React, { useState } from "react";
+import axios from "../lib/axiosConfig.js";
+import { CurrentState } from "./currentState.js";
 
 export const CacheApi = React.createContext();
 
-// TODO change name to cacheApi
 export function CacheApiProvider({ children }) {
   const [songsByAlbum, setSongsByAlbum] = useState({});
   const [artPiecesByAlbum, setArtPiecesByAlbum] = useState({});
   const [moments, setMoments] = useState([]);
+  const { setCurrentSong } = React.useContext(CurrentState);
 
-  //TODO move request here - verify if state has info - initializeSongsByAlbum
+  function initializeSongsByAlbum() {
+    if (Object.keys(songsByAlbum).length <= 0) {
+      const fetchData = async () => {
+        try {
+          const res = await axios.get(`/songs`);
+
+          let songsByAlbumObj = {};
+
+          for (const song of res.data) {
+            if (!Array.isArray(songsByAlbumObj[song.album_id])) {
+              songsByAlbumObj[song.album_id] = [];
+            }
+            songsByAlbumObj[song.album_id].push(song);
+          }
+
+          setSongsByAlbum(songsByAlbumObj);
+          setCurrentSong(res.data[0]);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      fetchData();
+    }
+  }
 
   return (
     <CacheApi.Provider
       value={{
         songsByAlbum,
-        setSongsByAlbum,
+        initializeSongsByAlbum,
         artPiecesByAlbum,
         setArtPiecesByAlbum,
         moments,
