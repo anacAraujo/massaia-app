e5b2e8edad91f385ebc0f8ac034d7763eb236373
anacAraujo <anacristinaferrazaraujo@gmail.com>
2024-06-15 00:31:40 +0100

    feat: change current song by controllers

--- client/src/components/MusicControllers.jsx
+++ client/src/components/MusicControllers.jsx
@@ -1,11 +1,36 @@
+import React, { useState } from "react";
 import "../styles/musicControllers.css";
+import { CacheApi } from "../context/cacheApi.js";
+import { CurrentState } from "../context/currentState.js";
 
 export default function MusicControllers() {
+  const { currentSong, setCurrentSong } = React.useContext(CurrentState);
+
+  const { songsById } = React.useContext(CacheApi);
+
+  function handlePrevSong() {
+    const keys = Object.keys(songsById);
+    let newSongId = keys.at(keys.indexOf(currentSong.id.toString()) - 1);
+    setCurrentSong(songsById[newSongId]);
+  }
+
+  function handleNextSong() {
+    const keys = Object.keys(songsById);
+    let newSongId = keys.at(keys.indexOf(currentSong.id.toString()) + 1);
+
+    if (newSongId === undefined) {
+      newSongId = keys[0];
+    }
+    setCurrentSong(songsById[newSongId]);
+  }
+
   return (
     <div className="musicControllers">
       <div className="left-group">
-        <button className="musicControllers-vol">vol. 1</button>
-        <p className="musicControllers-song-name">nome da música</p>
+        <button className="musicControllers-vol">
+          vol. {currentSong.album_id}
+        </button>
+        <p className="musicControllers-song-name">{currentSong.name}</p>
         <img
           className="musicControllers-more-songs"
           src="../assets/icons/arrow-up.png"
@@ -14,9 +39,13 @@ export default function MusicControllers() {
       </div>
 
       <div className="center-group">
-        <button className="control-button">Prev</button>
+        <button className="control-button" onClick={() => handlePrevSong()}>
+          Prev
+        </button>
         <button className="control-button">Play</button>
-        <button className="control-button">Next</button>
+        <button className="control-button" onClick={() => handleNextSong()}>
+          Next
+        </button>
       </div>
 
       <div className="right-group">
