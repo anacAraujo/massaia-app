cd05c1c54ffeb8997753f458d2c797c955983f65
anacAraujo <anacristinaferrazaraujo@gmail.com>
2024-06-14 16:34:08 +0100

    feat: add verification to only allow restiter by a logged in user

--- admin/src/pages/Register.js
+++ admin/src/pages/Register.js
@@ -1,84 +1,110 @@
-import React, { useContext } from 'react'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { CButton, CCard, CCardBody, CCol, CContainer, CForm, CFormInput, CInputGroup, CInputGroupText, CRow } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilLockLocked } from '@coreui/icons'
-import axios from '../lib/AxiosConfig'
-import Joi from 'joi';
+import { AuthContext } from "../context/AuthContext";
 
-const validTlds = ['com', 'pt', 'org', 'gov'];
+import React, { useContext } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import { cilLockLocked } from "@coreui/icons";
+import axios from "../lib/AxiosConfig";
+import Joi from "joi";
+
+const validTlds = ["com", "pt", "org", "gov"];
 
 const registerSchema = Joi.object({
-  email: Joi.string().email({
-    tlds: { allow: validTlds }
-  }).required().label('email'),
-  password: Joi.string().min(8).required().label('password'),
-  confirmPassword: Joi.any().equal(Joi.ref('password')).required().label('confirmPassword').messages({ 'any.only': 'As passwords não são iguais.' }) 
+  email: Joi.string()
+    .email({
+      tlds: { allow: validTlds },
+    })
+    .required()
+    .label("email"),
+  password: Joi.string().min(8).required().label("password"),
+  confirmPassword: Joi.any()
+    .equal(Joi.ref("password"))
+    .required()
+    .label("confirmPassword")
+    .messages({ "any.only": "As passwords não são iguais." }),
 });
 
 const Register = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const { currentUser } = useContext(AuthContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [validation, setValidation] = useState({});
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   const HandleEmailInput = (event) => {
     setEmail(event.target.value);
-  }
+  };
 
   const HandlePasswordInput = (event) => {
     setPassword(event.target.value);
-  }
+  };
 
   const HandleConfirmPassword = (event) => {
     setConfirmPassword(event.target.value);
-  }
+  };
 
   const ValidateForm = () => {
     const body = {
       email,
       password,
-      confirmPassword
-    }
+      confirmPassword,
+    };
 
     const { error } = registerSchema.validate(body, { abortEarly: false });
     if (error) {
       const errors = {};
-      error.details.forEach(err => {
+      error.details.forEach((err) => {
         errors[err.path[0]] = err.message;
-      })
+      });
       setValidation(errors);
       return false;
     }
     setValidation({});
     return true;
-  }
+  };
 
   const handleRegister = async (event) => {
     event.preventDefault();
     if (!ValidateForm()) {
-      return
+      return;
+    }
+
+    if (!currentUser) {
+      alert("Faça login para poder registar outra conta!");
+      return;
     }
 
     const body = {
       email,
       password,
-      confirmPassword
-    }
+      confirmPassword,
+    };
 
     try {
       await axios.post("/auth/register", body);
       navigate("/login");
-      alert('Sucesso! Faça login com as novas credenciais para entrar.');
+      alert("Sucesso! Faça login com as novas credenciais para entrar.");
     } catch (error) {
       setError(error.response);
       console.error(error);
-      
     }
-  }
+  };
 
   return (
     <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
@@ -88,14 +114,14 @@ const Register = () => {
             <CCard className="mx-4">
               <CCardBody className="p-4">
                 <CForm>
-                  <h1>Registo</h1>
-                  <p className="text-body-secondary">Cria a tua conta</p>
+                  <h1>Registar novo Utilizador</h1>
+                  <p className="text-body-secondary">Crie mais uma conta</p>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>@</CInputGroupText>
                     <CFormInput
-                      name='email'
+                      name="email"
                       type="text"
-                      placeholder="Email" 
+                      placeholder="Email"
                       onChange={HandleEmailInput}
                       required
                     />
@@ -106,30 +132,41 @@ const Register = () => {
                       <CIcon icon={cilLockLocked} />
                     </CInputGroupText>
                     <CFormInput
-                      name='password'
+                      name="password"
                       type="password"
                       placeholder="Password"
                       onChange={HandlePasswordInput}
                       required
                     />
                   </CInputGroup>
-                  {validation.password && <p>Tem que ter no mínimo 8 caracteres.</p>}
+                  {validation.password && (
+                    <p>Tem que ter no mínimo 8 caracteres.</p>
+                  )}
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
                       <CIcon icon={cilLockLocked} />
                     </CInputGroupText>
                     <CFormInput
-                      name='confirmPassword'
+                      name="confirmPassword"
                       type="password"
                       placeholder="Confirma a password"
                       onChange={HandleConfirmPassword}
                       required
                     />
                   </CInputGroup>
-                  {validation.confirmPassword && <p>{validation.confirmPassword}</p>}
+                  {validation.confirmPassword && (
+                    <p>{validation.confirmPassword}</p>
+                  )}
                   <div className="d-grid">
-                  {error && <p>Não foi possível efetuar o registo. Por favor volte a tentar!</p>}
-                    <CButton onClick={handleRegister} color="primary">Criar conta</CButton>
+                    {error && (
+                      <p>
+                        Não foi possível efetuar o registo. Por favor volte a
+                        tentar!
+                      </p>
+                    )}
+                    <CButton onClick={handleRegister} color="primary">
+                      Criar conta
+                    </CButton>
                   </div>
                 </CForm>
               </CCardBody>
@@ -138,7 +175,7 @@ const Register = () => {
         </CRow>
       </CContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
