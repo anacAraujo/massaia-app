15b6bfc621700b3285b1e8abf2246532a9411a62
anacAraujo <anacristinaferrazaraujo@gmail.com>
2024-06-14 16:31:35 +0100

    chore: add TODO delete icons

--- admin/src/components/AppSidebar.js
+++ admin/src/components/AppSidebar.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { CCloseButton, CSidebar, CSidebarBrand, CSidebarHeader, CSidebarNav, CNavTitle } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import { cilMusicNote, cilAlbum, cilHistory, cilWallpaper } from '@coreui/icons'
-import { logo } from '../assets/brand/logo'
-import { sygnet } from '../assets/brand/sygnet'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  CCloseButton,
+  CSidebar,
+  CSidebarBrand,
+  CSidebarHeader,
+  CSidebarNav,
+  CNavTitle,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import {
+  cilMusicNote,
+  cilAlbum,
+  cilHistory,
+  cilWallpaper,
+} from "@coreui/icons";
+import { logo } from "../assets/brand/logo";
+import { sygnet } from "../assets/brand/sygnet";
 
 const AppSidebar = () => {
-  
   const dispatch = useDispatch();
   const unfoldable = useSelector((state) => state.sidebarUnfoldable);
   const sidebarShow = useSelector((state) => state.sidebarShow);
@@ -21,43 +32,48 @@ const AppSidebar = () => {
       unfoldable={unfoldable}
       visible={sidebarShow}
       onVisibleChange={(visible) => {
-        dispatch({ type: 'set', sidebarShow: visible })
+        dispatch({ type: "set", sidebarShow: visible });
       }}
     >
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
+          {/* TODO delete icons */}
           <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
+          <CIcon
+            customClassName="sidebar-brand-narrow"
+            icon={sygnet}
+            height={32}
+          />
         </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
           dark
-          onClick={() => dispatch({ type: 'set', sidebarShow: false })}
+          onClick={() => dispatch({ type: "set", sidebarShow: false })}
         />
       </CSidebarHeader>
       <CSidebarNav>
         <CNavTitle>Tabelas</CNavTitle>
-        <div className='nav-item'>
-          <Link className='nav-link' to="/albums">
-            <CIcon className='nav-icon' icon={cilAlbum}></CIcon>
+        <div className="nav-item">
+          <Link className="nav-link" to="/albums">
+            <CIcon className="nav-icon" icon={cilAlbum}></CIcon>
             Álbuns
           </Link>
-          <Link className='nav-link' to="/Songs">
-            <CIcon className='nav-icon' icon={cilMusicNote}></CIcon>
+          <Link className="nav-link" to="/Songs">
+            <CIcon className="nav-icon" icon={cilMusicNote}></CIcon>
             Músicas
           </Link>
-          <Link className='nav-link' to="/art_pieces">
-          <CIcon className='nav-icon' icon={cilWallpaper}></CIcon>
+          <Link className="nav-link" to="/art_pieces">
+            <CIcon className="nav-icon" icon={cilWallpaper}></CIcon>
             Obras
           </Link>
-          <Link className='nav-link' to="/moments">
-          <CIcon className='nav-icon' icon={cilHistory}></CIcon>
+          <Link className="nav-link" to="/moments">
+            <CIcon className="nav-icon" icon={cilHistory}></CIcon>
             Momentos
           </Link>
         </div>
       </CSidebarNav>
     </CSidebar>
-  )
-}
+  );
+};
 
-export default AppSidebar
+export default AppSidebar;
