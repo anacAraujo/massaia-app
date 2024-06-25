915cd83980447f736591adce9e3d129caa1beb9f
anacAraujo <anacristinaferrazaraujo@gmail.com>
2024-06-25 15:43:21 +0100

    add: TODO

--- client/src/pages/Moments.jsx
+++ client/src/pages/Moments.jsx
@@ -9,7 +9,7 @@ export default function Moments() {
   const [scroll, setScroll] = useState(false);
   const [toggleHeader, setToggleHeader] = useState(true);
   console.log(moments);
-  
+
   useEffect(() => {
     initMoments();
     if (moments.length > 3) {
@@ -19,7 +19,9 @@ export default function Moments() {
 
   useEffect(() => {
     const handleScroll = () => {
-      const momentsContainer = document.querySelector('.moments-grid-container');
+      const momentsContainer = document.querySelector(
+        ".moments-grid-container"
+      );
       if (momentsContainer.scrollTop > 0) {
         setToggleHeader(false);
       } else {
@@ -27,27 +29,32 @@ export default function Moments() {
       }
     };
 
-    const momentsContainer = document.querySelector('.moments-grid-container');
+    const momentsContainer = document.querySelector(".moments-grid-container");
     if (momentsContainer) {
-      momentsContainer.addEventListener('scroll', handleScroll);
+      momentsContainer.addEventListener("scroll", handleScroll);
     }
 
     return () => {
       if (momentsContainer) {
-        momentsContainer.removeEventListener('scroll', handleScroll);
+        momentsContainer.removeEventListener("scroll", handleScroll);
       }
     };
   }, []);
 
+  //TODO add loading spinner (same as gallery)
   return (
     <div className="moments-container">
-      <div className={`header-container ${toggleHeader ? 'show' : 'hide'}`}>
+      <div className={`header-container ${toggleHeader ? "show" : "hide"}`}>
         <Header />
       </div>
-      <div className={`moments-grid-container ${scroll === true ? 'scrollable' : ''}`}>
+      <div
+        className={`moments-grid-container ${
+          scroll === true ? "scrollable" : ""
+        }`}
+      >
         <div className="moments-grid">
           {Array.isArray(moments) && moments.length > 0 ? (
-            moments.map((moment) => (
+            moments.map((moment) =>
               moment.image !== null ? (
                 <img
                   key={moment.id}
@@ -55,14 +62,14 @@ export default function Moments() {
                   alt={moment.name}
                 />
               ) : (
-                <video 
+                <video
                   controls
                   key={moment.id}
                   src={`${process.env.REACT_APP_UPLOAD_FOLDER}${moment.video}`}
                   alt={moment.name}
                 />
               )
-            ))
+            )
           ) : (
             <p>Ainda não há momentos disponíveis.</p>
           )}
