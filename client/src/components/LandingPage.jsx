008f57c22d2038a16a22ea85209fd31dddb23804
anacAraujo <anacristinaferrazaraujo@gmail.com>
2024-07-02 15:02:48 +0100

    feat: add new images for landing page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -10,7 +10,12 @@ export function LandingPage() {
         <h1>MASSAIÁ</h1>
         <p>espaços da voz, do som e do olhar</p>
         <div className="content">
-          <img src="../assets/images/landing-page-text.png" alt="Massaiá" />
+          <img src="../assets/images/massaiaintro1.png" alt="Massaiá" />
+          <img src="../assets/images/massaiaintro2.png" alt="Massaiá" />
+
+          <img src="../assets/images/massaiaintro3.png" alt="Massaiá" />
+
+          <img src="../assets/images/massaiaintro4.png" alt="Massaiá" />
         </div>
       </div>
       <div className="construction-notice">
