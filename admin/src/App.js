72d30e69b0de26efe5047a756754396bc535e924
anacAraujo <anacristinaferrazaraujo@gmail.com>
2024-07-02 22:22:56 +0100

    chore: clean folders and fix menu position

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,17 +1,15 @@
-import './scss/style.scss'
-import { BrowserRouter } from 'react-router-dom'
-import AppRoutes from './routes';
-import { AuthContextProvider } from './context/AuthContext';
-
+import "./scss/style.scss";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <AppRoutes>
-        </AppRoutes>
+        <AppRoutes></AppRoutes>
       </AuthContextProvider>
-    </BrowserRouter>   
+    </BrowserRouter>
   );
 }
 
